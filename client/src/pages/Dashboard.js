42434f63d29595cb5c167094fa3a6a78ab9320cd
Chidimma <chidimmamofodum@gmail.com>
2023-02-17 15:29:46 +0000

    abstracted components to files

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Chart, registerables } from "chart.js";
-import { BsFilterSquare, BsTelephoneFill } from "react-icons/bs";
-import { TiMessages } from "react-icons/ti";
-import { AiTwotoneHome } from "react-icons/ai";
-
+import "./Dashboard.css";
+import Nav from "../Components/Nav";
+import Title from "../Components/Title";
 import BarChart from "../Components/Bar";
 import PieChart from "../Components/pie";
-import "./Dashboard.css";
-import { Link } from "react-router-dom";
+import MsgStats from "../Components/MsgStats";
+import Footer from "../Components/Footer";
 Chart.register(...registerables);
 
 const channels = [
@@ -80,40 +79,11 @@ const Dashboard = () => {
 	};
 	return (
 		<div className="Dashboard">
-			<div className="nav">
-				<BsFilterSquare
-					className="toggle"
-					size={50}
-					onClick={pieOrBar}
-				></BsFilterSquare>
-				<h2>DASHBOARD</h2>
-				<div className="profile-pics"></div>
-			</div>
-			<div className="title">
-				<h2>Weekly Overview</h2>
-			</div>
-			{isBar ? (
-				<BarChart channels={channels} />
-			) : (
-				<PieChart channels={channels} />
-			)}
-			<div className="msg-stats">
-				<div className="icons">
-					<TiMessages size={80} color={"green"} />
-					<p>20</p>
-				</div>
-				<div className="icons">
-					<BsTelephoneFill size={70} color={"#28cef4"} />
-					<p>14</p>
-				</div>
-			</div>
-			<footer>
-				<Link to= "/">
-				<button className="homebtn-wrapper">
-					<AiTwotoneHome size={40} color={"red"}></AiTwotoneHome>
-				</button>
-				</Link>
-			</footer>
+			<Nav onClick={pieOrBar} />
+			<Title />
+			{isBar ? (<BarChart channels={channels} />) : (<PieChart channels={channels} />)}
+			<MsgStats />
+			<Footer />
 		</div>
 	);
 };
