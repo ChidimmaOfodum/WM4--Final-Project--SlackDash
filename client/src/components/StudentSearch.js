5f3ca3b3885ab735f289e67e5b6dad08f935f4ec
michelle <mindyourphotosynthesis@gmail.com>
2023-02-18 14:07:06 +0000

    student-view: updated

--- client/src/components/StudentSearch.js
+++ client/src/components/StudentSearch.js
@@ -1,8 +1,13 @@
+import { BsSortDown } from "react-icons/bs";
+import { BsSortUpAlt } from "react-icons/bs";
+
 const StudentSearch = () => {
 
 	return (
 		<div>
 			<input className="student-search" type="text" placeholder="&#61442;" />
+			<BsSortDown />
+			<BsSortUpAlt />
 		</div>
 	);
 };
