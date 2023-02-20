b7500c7e2c78cc45b1c504070e147e1e2161c78a
Chidimma <chidimmamofodum@gmail.com>
2023-02-20 14:11:36 +0000

    abstracting all slack methods to a separate file

--- server/api.js
+++ server/api.js
@@ -1,83 +1,12 @@
 import { Router } from "express";
-import getSlackClient from "./slackClient";
 import logger from "./utils/logger";
 
 const router = Router();
-const client = getSlackClient();
 
-// 
+router.get("/messages", async(req, res) => {
+	const { members } = await channelMembers();
 
-// Giving all channels informations
-async function allChannelsInfo() {
-	try {
-		// Call the conversations.list method using the built-in WebClient
-		return await client.conversations.list({
-			// The token you used to initialize your app
-			token: process.env.TOKEN,
-		});
-	} catch (error) {
-		console.error(error);
-	}
-}
-router.get("/channels", async(_, res) => {
-	let result = await allChannelsInfo();
-	res.send(result);
-});
 
-// Giving all channel member IDs
-async function channelMembers() {
-	try {
-		// Call the conversations.list method using the built-in WebClient
-		return await client.conversations.members({
-			channel: process.env.CHANNEL_ID,
-			// The token you used to initialize your app
-			token: process.env.TOKEN,
-		});
-	} catch (error) {
-		console.error(error);
-	}
-}
-router.get("/channelMembers", async(_, res) => {
-	let result = await channelMembers();
-	res.send(result);
-});
-
-// User presence by ID
-async function memberPresence(req) {
-	try {
-		// Call the conversations.list method using the built-in WebClient
-		return await client.users.getPresence({
-			token: process.env.TOKEN,
-			user: req.params.id,
-			// The token you used to initialize your app
-		});
-	} catch (error) {
-		console.error(error);
-	}
-}
-router.get("/memberPresence/:id", async(req, res) => {
-	let result = await memberPresence(req);
-	res.send(result);
-});
-
-// History of conversation in a channel, it shows who involved the chat
-async function channelConversationHistory() {
-	try {
-		// Call the conversations.list method using the built-in WebClient
-		return await client.conversations.history({
-			token: process.env.TOKEN,
-			channel: process.env.CHANNEL_ID,
-			//user: req.params.id,
-			// The token you used to initialize your app
-		});
-	} catch (error) {
-		console.error(error);
-	}
-}
-router.get("/conversations/channel/:id", async(req, res) => {
-	let result = await channelConversationHistory(req);
-	res.send(result);
-});
 
 router.get("/", (_, res) => {
 	logger.debug("Welcoming everyone...");
---
+++ server/slackMethods.js
@@ -0,0 +1,38 @@
+import getSlackClient from "./slackClient";
+const client = getSlackClient();
+
+//get all members in a given channel
+export async function getChannelMembers() {
+	try {
+		return await client.conversations.members({
+			channel: process.env.CHANNEL_ID,
+			token: process.env.TOKEN,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+//Given a userId, this function returns detailed info of the user.
+export async function getUserInfo(userId) {
+	try {
+		return await client.users.info({
+			token: process.env.TOKEN,
+			user: userId,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+//returns all messages in a particular channel
+export async function getChannelMessages() {
+	try {
+		return await client.conversations.history({
+			token: process.env.TOKEN,
+			channel: process.env.CHANNEL_ID,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+}
