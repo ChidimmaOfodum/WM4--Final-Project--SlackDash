d4ae66a4455c4987eac08cb1ee336b0582c25e99
michellejanay <mindyourphotosynthesis@gmail.com>
2023-02-24 13:30:57 +0000

    refactored: working

--- client/src/Components/StudentView/Student.js
+++ client/src/Components/StudentView/Student.js
@@ -3,7 +3,6 @@ import "./Students.css";
 const Student = ( { students } ) => {
 	return (
 			<tbody>{Object.keys(students).map((student, i) => (
-			<tbody>{Object.keys(students).map((student, i) => (
 				<tr className="student-table-view" key={i}>
 					<td><img src={students[student][0].image_32} alt={students[student][0].name} className="student-img" /></td>
 					<td>{students[student][0].name}</td>
--- client/src/Components/StudentView/StudentTable.js
+++ client/src/Components/StudentView/StudentTable.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useState, useEffect } from "react";
 import Student from "./Student";
 
 const StudentTable = () => {
