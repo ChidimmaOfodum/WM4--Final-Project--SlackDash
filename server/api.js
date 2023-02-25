ae83fa5725981cbbf78bf9804031fda9fc75b86a
Chidimma <chidimmamofodum@gmail.com>
2023-02-25 01:35:03 +0000

    changing the structure of the data to include all members of a channel as opposed to members who have sent at least a message

--- server/api.js
+++ server/api.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import logger from "./utils/logger";
 import getData from "./Controllers/getData";
+import getSlackClient from "./slackClient";
 import {  getChannelMembers, getUserInfo } from "./slackMethods";
 
 const router = Router();
-
+const client = getSlackClient();
 router.get("/", (_, res) => {
 	logger.debug("Welcoming everyone...");
 	res.json({ message: "Hello, world!" });
@@ -13,8 +14,20 @@ router.get("/", (_, res) => {
 router.get("/data", getData);
 
 router.get("/test", async (req, res) => {
-	const data = await getUserInfo("U04PWALHADP");
-	res.json(data);
+	//const data = await getUserInfo("U04PWALHADP");
+	let response;
+
+	try {
+		response = await client.conversations.join({
+			token: process.env.TOKEN,
+			channel: "C04RUN1MBR6",
+		});
+	} catch (error) {
+		console.log(error);
+	}
+
+	res.json(response);
 });
+
 
 export default router;
