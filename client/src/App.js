b4b6d407538166cb3dbd02444f4627a81a584fd4
michellejanay <mindyourphotosynthesis@gmail.com>
2023-02-26 22:50:11 +0000

    landingpage: updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
-import Home from "./pages/Home";
 import StudentView from "./pages/StudentView";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
@@ -10,13 +9,12 @@ import LandingPage from "./pages/LandingPage";
 
 const App = () => (
 	<Routes>
-		<Route path="/" element={<Home />} />
+		<Route path="/" element={<LandingPage />} />
 		<Route path="/about/this/site" element={<About />} />
 		<Route path="/students/table/view" element={<StudentView />} />
 		<Route path="/dashboard" element={<Dashboard />} />
 		<Route path="/login" element={<Login />} />
 		<Route path="/signup" element={<SignUp />} />
-		<Route path="/landingpage" element={<LandingPage />} />
 	</Routes>
 );
 
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -7,13 +7,21 @@ const LandingPage = () => {
 	return (
 		<main className="landing-main">
 			<img src={SlackDashLogo} alt="SlackDash" className="slackdash-logo" />
-      <h6>An app for tracking trainee communication data in CYF Slack Workspace</h6>
+			<div className="links">
+				<Link to="/students/table/view" className="links">Student Table View</Link>
+				<Link to="/dashboard" className="links">Dashboard</Link>
+				<Link to="/login" className="links">Login to your account</Link>
+				<Link to="/signup" className="links">Create a new account</Link>
+			</div>
+			<h6>
+				An app for tracking trainee communication data in CYF Slack Workspace
+			</h6>
 			<section>
 				<Link to="/login">
-					<button className="btn btn-danger">Login</button>
+					<button className="btn btn-light">Login</button>
 				</Link>
 				<Link to="/signup">
-					<button className="btn btn-danger">Sign Up</button>
+					<button className="btn btn-light">Sign Up</button>
 				</Link>
 			</section>
 		</main>
