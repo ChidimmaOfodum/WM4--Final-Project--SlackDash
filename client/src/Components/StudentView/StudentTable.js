1a8cd49d5821fdcff888cc786ff4b5d76e529a74
michellejanay <mindyourphotosynthesis@gmail.com>
2023-02-26 17:41:59 +0000

    loading: added

--- client/src/Components/StudentView/StudentTable.js
+++ client/src/Components/StudentView/StudentTable.js
@@ -2,29 +2,31 @@ import { useState, useEffect } from "react";
 import Student from "./Student";
 
 const StudentTable = () => {
-  const [students, setStudents] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [students, setStudents] = useState([]);
 
-  useEffect(() => {
-	fetch("/api/data")
-	.then((res) => res.json())
-	.then((data) => setStudents(data))
-	.catch((err) => console.log(err));
-}, []);
+	useEffect(() => {
+		fetch("/api/data")
+			.then((res) => res.json())
+			.then((data) => setStudents(data))
+			.then(() => setLoading(false))
+			.catch((err) => console.log(err));
+	}, []);
 
-  return (
-    <table>
-      <thead>
-			<tr>
-				<th>Image</th>
-				<th>Name</th>
-				<th>Total Messages</th>
-				<th>Total Calls</th>
-				<th>Time of Last Message</th>
-			</tr>
-      </thead>
-      <Student students={students} />
-      </table>
-  );
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Image</th>
+					<th>Name</th>
+					<th>Total Messages</th>
+					<th>Total Calls</th>
+					<th>Time of Last Message</th>
+				</tr>
+			</thead>
+			{loading ? <>Loading...</> : <Student students={students} />}
+		</table>
+	);
 };
 
 export default StudentTable;
