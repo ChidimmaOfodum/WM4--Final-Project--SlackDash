1837ad2890048b5bb1bf49efa46ae6d5859e046d
michellejanay <mindyourphotosynthesis@gmail.com>
2023-02-26 16:07:46 +0000

    modal: alternative

--- client/src/Components/StudentView/StudentSearch.js
+++ client/src/Components/StudentView/StudentSearch.js
@@ -1,13 +1,14 @@
 import { BsSortDown } from "react-icons/bs";
 import { BsSortUpAlt } from "react-icons/bs";
+import {BsPlusLg} from 'react-icons/bs'
 
-const StudentSearch = () => {
+const StudentSearch = ({handleShow}) => {
 
 	return (
 		<div className="search-sort-buttons">
-			<input className="student-search" type="text" placeholder="&#61442;" />
 			<BsSortDown />
-			<BsSortUpAlt />
+			{/* <BsSortUpAlt /> */}
+			<span>Add a channel <BsPlusLg onClick={handleShow}/></span>
 		</div>
 	);
 };
