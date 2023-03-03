d84937583fbf993985fb2c17540fe361d76f773d
Chidimma <chidimmamofodum@gmail.com>
2023-03-03 17:03:40 +0000

    adding cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import apiRouter from "./api";
 import config from "./utils/config";
@@ -16,6 +17,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 app.use(configuredHelmet());
 app.use(configuredMorgan());
 
