0cb73e237ab76df727d911e5f950aca241d7f667
farzad-nazif <farzadnazif.nz@gmail.com>
2023-03-04 22:23:31 +0000

    buttons fully working

--- client/src/Components/StudentView/StudentSearch.js
+++ client/src/Components/StudentView/StudentSearch.js
@@ -1,13 +1,23 @@
 import { BsSortDown } from "react-icons/bs";
 import { BsSortUpAlt } from "react-icons/bs";
 
-const StudentSearch = ({}) => {
+const StudentSearch = ({prevWeekChangeHandle , nextWeekChangeHandle, updatedNextSaturday , updatedPreviousSaturday}) => {
+
+	const epochConversion = (timestamp) => {
+		const date = new Date(timestamp*1000);
+		const day = date.getDate();
+		const month = date.getMonth() + 1; // Note: monh values start from 0, so we add 1 to get the correct month value
+		const year = date.getFullYear();
+		let result = `${day}-${month}-${year}`; // Output: "4-3-2022"
+		return result;
+	};
+
 	return (
 		<div className="search-sort-buttons">
 			<section className="week-of">
-			   <span className="arrowsUp">{"<"}</span>
-				<p>Week of: dategoeshere</p>
-			    <span className="arrowsUp">{">"}</span>
+			   <span className="arrowsUp" onClick={prevWeekChangeHandle} >{"<"}</span>
+				<p>{`Week between: ${epochConversion(updatedPreviousSaturday)} and ${epochConversion(updatedNextSaturday)} `}</p>
+			    <span className="arrowsUp" onClick={nextWeekChangeHandle} >{">"}</span>
 			</section>
 			<BsSortDown />
 			{/* <BsSortUpAlt /> */}
