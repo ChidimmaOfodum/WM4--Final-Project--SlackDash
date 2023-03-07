ffddd2eb1e4de1f983c9714a5740d03e34b1e5e1
Chidimma <chidimmamofodum@gmail.com>
2023-03-07 11:33:06 +0000

    profile data functional

--- server/Controllers/studentProfileData.js
+++ server/Controllers/studentProfileData.js
@@ -1,64 +1,5 @@
-import getSlackClient from "./slackClient";
-import db from "./db";
-const client = getSlackClient();
+import db from "../db";
 
-//get all members in a given channel
-export async function getChannelMembers(channelID) {
-	try {
-		return await client.conversations.members({
-			channel: channelID,
-			token: process.env.TOKEN,
-		});
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-//Given a userId, this function returns detailed info of the user.
-export async function getUserInfo(userId) {
-	try {
-		return await client.users.info({
-			token: process.env.TOKEN,
-			user: userId,
-		});
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-//returns all messages in a particular channel
-
-export async function getChannelData(channelID, oldest, latest) {
-	try {
-		return await client.conversations.history({
-			token: process.env.TOKEN,
-			channel: channelID,
-			oldest: oldest,
-			latest: latest
-		});
-	} catch(error){
-		console.log(error);
-	}
-}
-
-//return all replies of a channel for a given thread_ts
-
-export async function getReplies(channelId, thread_ts) {
-	try {
-		return await client.conversations.replies({
-			token: process.env.TOKEN,
-			channel: channelId,
-			ts: thread_ts,
-			//oldest: 1676377207.351409,
-		});
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-
-
-// accept a student ID and give me total messages in each channel 
 export async function studentProfileData(userId) {
 	try {
 		let totalMessages = 0;
@@ -73,7 +14,7 @@ export async function studentProfileData(userId) {
 			let channelName = channelDetails.channel_name;
 			var totalMessagesPerChannel = 0;
 			var totalCallsForEachChannel = 0;
-
+      
 			const rawChannelMessages = await client.conversations.history({
 				token: process.env.TOKEN,
 				channel: channelID,
