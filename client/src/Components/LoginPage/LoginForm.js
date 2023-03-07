1efd6a011811d88b1668377653c18f05abd05edf
michellejanay <mindyourphotosynthesis@gmail.com>
2023-03-07 21:02:12 +0000

    hashing for login/signup: completed

--- client/src/Components/LoginPage/LoginForm.js
+++ client/src/Components/LoginPage/LoginForm.js
@@ -1,34 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import { useRef } from "react";
 import "../../pages/Login.css";
+import bcrypt from "bcryptjs";
 
-function LoginForm() {
-  // I have just added this click handle functions to use for future purposes
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const LoginForm = () => {
+	const emailRef = useRef();
+	const passwordRef = useRef();
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const email = emailRef.current.value;
+		const password = bcrypt.hashSync(passwordRef.current.value, 10);
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+		fetch("/login", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			}),
+		})
+			.then((res) => res.json())
+			.catch((err) => console.log(err));
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
-  };
-  return (
-    <div className='userDetail'>
-     <form onSubmit={handleSubmit} className='loginForm' >
-      <label htmlFor='email'>Email</label>
-      <input type="email" value={email} onChange={handleEmailChange} className="userInput" id='email' />
-      <label htmlFor='password' >Password</label>
-      <input type="password" value={password} onChange={handlePasswordChange} className="userInput" id='password'/>
-      <button type="submit" className='submitBtn' >Continue</button>
-     </form>
-   </div>
-  )
-}
+		console.log({ email: email, password: password });
+	};
 
-export default LoginForm
+	return (
+		<div className="userDetail">
+			<form onSubmit={handleSubmit} className="loginForm">
+				<label htmlFor="email">Email</label>
+				<input type="email" ref={emailRef} className="userInput" id="email" />
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					ref={passwordRef}
+					className="userInput"
+					id="password"
+				/>
+				<button type="submit" className="submitBtn">
+					Continue
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default LoginForm;
--- client/src/Components/SignUpPage/SignUpForm.js
+++ client/src/Components/SignUpPage/SignUpForm.js
@@ -1,21 +1,47 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from "react";
 import "../../pages/SignUp.css";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 
-function SignUpForm() {
-	const firstName = useRef()
-	const lastName = useRef()
-	const emailInputRef = useRef()
-	const passwordInputRef = useRef()
-	const traineeOrMentor = useRef()
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-	  const email = emailInputRef.current.value
-	  const password = bcrypt.hashSync(passwordInputRef.current.value, 10)
-	  console.log(password)
-    };  
-  return (
+const SignUpForm = () => {
+	const firstName = useRef();
+	const lastName = useRef();
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
+	const [selected, setSelected] = useState();
+
+	const onRadioSelect = (e) => {
+		setSelected(e.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const email = emailInputRef.current.value;
+		const password = bcrypt.hashSync(passwordInputRef.current.value, 10);
+
+		fetch("/signUp", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				firstName: firstName.current.value,
+				lastName: lastName.current.value,
+				email: email,
+				password: password,
+				traineeOrMentor: selected,
+			}),
+		})
+			.then((res) => res.json())
+			.catch((err) => console.log(err));
+
+		console.log({
+			firstName: firstName.current.value,
+			lastName: lastName.current.value,
+			email: email,
+			password: password,
+			traineeOrMentor: selected,
+		});
+	};
+
+	return (
 		<div className="userDetail">
 			<form onSubmit={handleSubmit} className="signUpForm">
 				<label htmlFor="firstName">First Name</label>
@@ -24,7 +50,7 @@ function SignUpForm() {
 					ref={firstName}
 					className="userInput"
 					id="firstName"
-					name='first-name'
+					name="first-name"
 				/>
 				<label htmlFor="lastName">Last Name</label>
 				<input
@@ -32,7 +58,7 @@ function SignUpForm() {
 					ref={lastName}
 					className="userInput"
 					id="lastName"
-					name='last-name'
+					name="last-name"
 				/>
 				<label htmlFor="eamil">Email</label>
 				<input
@@ -40,7 +66,7 @@ function SignUpForm() {
 					ref={emailInputRef}
 					className="userInput"
 					id="email"
-					name='email'
+					name="email"
 				/>
 				<label htmlFor="password">Password</label>
 				<input
@@ -48,27 +74,38 @@ function SignUpForm() {
 					ref={passwordInputRef}
 					className="userInput"
 					id="password"
-					name='password'
+					name="password"
 				/>
 
-        <div className='radioBtnWrapper'>
-          <div className='radioBtn'>
-				<input type="radio" id="mentor" name='input' />
-				<label htmlFor="mentor">Mentor</label>
-          </div>
+				<div className="radioBtnWrapper">
+					<div className="radioBtn">
+						<input
+							type="radio"
+							id="mentor"
+							name="input"
+							value="mentor"
+							onChange={onRadioSelect}
+						/>
+						<label htmlFor="mentor">Mentor</label>
+					</div>
 
-          <div className='radioBtn'>
-				<input type="radio" id="trainee" name='input'/>
-				<label htmlFor="trainee">Trainee</label>
-          </div>
-
-        </div>
+					<div className="radioBtn">
+						<input
+							type="radio"
+							id="trainee"
+							name="input"
+							value="trainee"
+							onChange={onRadioSelect}
+						/>
+						<label htmlFor="trainee">Trainee</label>
+					</div>
+				</div>
 				<button type="submit" className="signUpBtn">
 					Continue
 				</button>
 			</form>
 		</div>
 	);
-}
+};
 
-export default SignUpForm
+export default SignUpForm;
