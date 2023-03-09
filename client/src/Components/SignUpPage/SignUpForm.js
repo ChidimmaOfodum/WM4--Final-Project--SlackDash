5c0c2ede860cc78281c6abef9409b3ac36306978
Chidimma <chidimmamofodum@gmail.com>
2023-03-09 19:20:11 +0000

    adding password validator

--- client/src/Components/SignUpPage/SignUpForm.js
+++ client/src/Components/SignUpPage/SignUpForm.js
@@ -13,11 +13,18 @@ const SignUpForm = () => {
 		setSelected(e.target.value);
 	};
 
+	const passwordValidation = (event) => {
+		const passwordRegex =
+			/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+		const inputPassword = event.target.value;
+		return passwordRegex.test(inputPassword);
+	};
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		const email = emailInputRef.current.value;
 		// const password = bcrypt.hashSync(passwordInputRef.current.value, 10);
-		const password = passwordInputRef.current.value
+		const password = passwordInputRef.current.value;
 
 		fetch("/signUp", {
 			method: "POST",
@@ -35,47 +42,47 @@ const SignUpForm = () => {
 	};
 
 	return (
-			<form onSubmit={handleSubmit} className="signup-form">
-				<label htmlFor="firstName">First Name</label>
-				<input
-					type="text"
-					ref={firstName}
-					className="userInput"
-					id="firstName"
-					name="first-name"
-					required
-				/>
-				<label htmlFor="lastName">Last Name</label>
-				<input
-					type="text"
-					ref={lastName}
-					className="userInput"
-					id="lastName"
-					name="last-name"
-					required
-				/>
-				<label htmlFor="eamil">Email</label>
-				<input
-					type="email"
-					ref={emailInputRef}
-					className="userInput"
-					id="email"
-					name="email"
-					required
-				/>
-				<label htmlFor="password">Password</label>
-				<input
-					type="password"
-					ref={passwordInputRef}
-					className="userInput"
-					id="password"
-					name="password"
-					required
-				/>
+		<form onSubmit={handleSubmit} className="signup-form">
+			<label htmlFor="firstName">First Name</label>
+			<input
+				type="text"
+				ref={firstName}
+				className="userInput"
+				id="firstName"
+				name="first-name"
+				required
+			/>
+			<label htmlFor="lastName">Last Name</label>
+			<input
+				type="text"
+				ref={lastName}
+				className="userInput"
+				id="lastName"
+				name="last-name"
+				required
+			/>
+			<label htmlFor="eamil">Email</label>
+			<input
+				type="email"
+				ref={emailInputRef}
+				className="userInput"
+				id="email"
+				name="email"
+				required
+			/>
+			<label htmlFor="password">Password</label>
+			<input
+				type="password"
+				ref={passwordInputRef}
+				className="userInput"
+				id="password"
+				name="password"
+				required
+			/>
 
-				<fieldset className="radioBtnWrapper">
-					<legend>Please select your role</legend>
-					<div className="radio-style-helper">
+			<fieldset className="radioBtnWrapper">
+				<legend>Please select your role</legend>
+				<div className="radio-style-helper">
 					<div className="radioBtn">
 						<input
 							type="radio"
@@ -97,12 +104,12 @@ const SignUpForm = () => {
 						/>
 						<label htmlFor="trainee">Trainee</label>
 					</div>
-					</div>
-				</fieldset>
-				<button type="submit" className="signup-btn btn btn-danger">
-					Continue
-				</button>
-			</form>
+				</div>
+			</fieldset>
+			<button type="submit" className="signup-btn btn btn-danger">
+				Continue
+			</button>
+		</form>
 	);
 };
 
