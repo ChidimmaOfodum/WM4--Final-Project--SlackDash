a175d173c10a999182347c85faee4197e4a106e1
farzad-nazif <farzadnazif.nz@gmail.com>
2023-03-09 23:41:10 +0000

    role has been added to table

--- server/Controllers/models/user.js
+++ server/Controllers/models/user.js
@@ -10,6 +10,10 @@ const User = sequelize.define("User", {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
--- server/Controllers/signUpUser.js
+++ server/Controllers/signUpUser.js
@@ -4,7 +4,7 @@ const User = require("./models/user");
 const signUpUser = async (req,res) => {
     try{
         
-  const { firstName, lastName, email, password } = req.body;
+  const { firstName, lastName,role, email, password } = req.body;
 
   const alreadyExistsUser = await User.findOne({ where: { email } }).catch(
     (err) => {
@@ -16,7 +16,7 @@ const signUpUser = async (req,res) => {
     return res.status(409).json({ message: "User with email already exists!" });
   }
 
-  const newUser = new User({ firstName, lastName, email, password });
+  const newUser = new User({ firstName, lastName,role, email, password });
   const savedUser = await newUser.save().catch((err) => {
     console.log("Error: ", err);
     res.status(500).json({ error: "Cannot register user at the moment!" });
