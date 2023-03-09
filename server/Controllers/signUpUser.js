ad8c4b6328dcadf21c4b8755c56d1a53edbbacdb
farzad-nazif <farzadnazif.nz@gmail.com>
2023-03-09 23:22:33 +0000

    login/signUp function added/fully working

---
+++ server/Controllers/signUpUser.js
@@ -0,0 +1,34 @@
+const User = require("./models/user");
+
+
+const signUpUser = async (req,res) => {
+    try{
+        
+  const { firstName, lastName, email, password } = req.body;
+
+  const alreadyExistsUser = await User.findOne({ where: { email } }).catch(
+    (err) => {
+      console.log("Error: ", err);
+    }
+  );
+
+  if (alreadyExistsUser) {
+    return res.status(409).json({ message: "User with email already exists!" });
+  }
+
+  const newUser = new User({ firstName, lastName, email, password });
+  const savedUser = await newUser.save().catch((err) => {
+    console.log("Error: ", err);
+    res.status(500).json({ error: "Cannot register user at the moment!" });
+  });
+
+  if (savedUser) res.json({ message: "Thanks for registering" });
+       
+       res.send(email)
+    } catch(err){
+        console.log(err);
+    }
+
+}
+
+export default signUpUser;
