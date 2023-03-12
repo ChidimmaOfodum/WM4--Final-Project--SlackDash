8b023d3757cb072faece78a8cd7c3ecb6a8f9a9c
farzad-nazif <farzadnazif.nz@gmail.com>
2023-03-12 12:58:39 +0000

    users can login and see their profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import { Route, Routes } from "react-router-dom";
+import { CookiesProvider } from 'react-cookie';
 
 import About from "./pages/About";
 import StudentView from "./pages/StudentView";
 import Dashboard from "./pages/Dashboard";
+import TraineeDashboard from "./pages/TraineeDashboard";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import LandingPage from "./pages/LandingPage";
 
 const App = () => (
+	<CookiesProvider>
 	<Routes>
 		<Route path="/" element={<LandingPage />} />
 		<Route path="/about/this/site" element={<About />} />
 		<Route path="/students/table/view" element={<StudentView />} />
 		<Route path="/dashboard" element={<Dashboard />} />
+		<Route path="/traineedashboard" element={<TraineeDashboard />} />
 		<Route path="/login" element={<Login />} />
 		<Route path="/signup" element={<SignUp />} />
 	</Routes>
+	</CookiesProvider>
 );
 
 export default App;
