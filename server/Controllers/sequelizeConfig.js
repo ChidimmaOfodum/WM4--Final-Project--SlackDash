2f2b52800ec336caa25944fcfda3e93a4ac238ca
Chidimma <chidimmamofodum@gmail.com>
2023-03-14 16:48:55 +0000

    sequelize connection test

--- server/Controllers/sequelizeConfig.js
+++ server/Controllers/sequelizeConfig.js
@@ -4,7 +4,10 @@ const sequelize = new Sequelize(process.env.DATABASE_INTERNAL, {
   dialect: 'postgres',
   ssl: true,
   dialectOptions: {
-    ssl: true
+    ssl: {
+      "require": true,
+      "rejectUnauthorized": false
+    }
   }
 });
 
