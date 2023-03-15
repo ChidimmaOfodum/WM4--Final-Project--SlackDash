eb1fcf136875decda513bb1fcb7556a7761b964f
michellejanay <mindyourphotosynthesis@gmail.com>
2023-03-15 15:18:46 +0000

    style: updated

--- client/src/Components/StudentView/ChannelSelect.js
+++ client/src/Components/StudentView/ChannelSelect.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import { BsPlusLg } from "react-icons/bs";
 
-const ChannelSelect = ({channelName, handleChange, handleShow, handleClick}) => {
+const ChannelSelect = ({
+	channelName,
+	handleChange,
+	handleShow,
+	handleClick,
+}) => {
 	const [channels, setChannels] = useState([]);
-
-
 
 	useEffect(() => {
 		fetch("/api/channels")
@@ -15,16 +18,29 @@ const ChannelSelect = ({channelName, handleChange, handleShow, handleClick}) => 
 
 	return (
 		<div className="channels-section">
-			<select name="channels" id="channels" onChange={handleChange} defaultValue = {"default"}>
-				<option value={"default"} disabled>Select a channel</option>
+			<select
+				name="channels"
+				id="channels"
+				onChange={handleChange}
+				defaultValue={"default"}
+			>
+				<option value={"default"} disabled>
+					Select a channel
+				</option>
 				{channels.map((channel, i) => (
-					<option value={channel} key= {i}>{channel}</option>
+					<option value={channel} key={i}>
+						{channel}
+					</option>
 				))}
 			</select>
-			<button className="btn btn-danger" onClick={handleClick}>View Data</button>
-			<span>
-				Add a channel <BsPlusLg onClick={handleShow} />
-			</span>
+			<section className="view-add-btns">
+				<button className="btn btn-danger" onClick={handleClick}>
+					View Data
+				</button>
+				<button className="btn btn-danger" onClick={handleShow}>
+					Add a channel <BsPlusLg />
+				</button>
+			</section>
 		</div>
 	);
 };
