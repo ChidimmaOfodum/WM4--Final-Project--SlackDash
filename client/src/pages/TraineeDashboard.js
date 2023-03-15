899e9e0edfd266cca6dbc2370afbe31543bec84c
Chidimma <chidimmamofodum@gmail.com>
2023-03-15 04:14:09 +0000

    added weekly time frame for trainees

--- client/src/pages/TraineeDashboard.js
+++ client/src/pages/TraineeDashboard.js
@@ -1,4 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { startOfWeek, endOfWeek } from "date-fns";
+import { getUnixTime } from "date-fns";
+import { DateRangePicker } from "react-date-range";
+import { format } from "date-fns";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 import { Chart, registerables } from "chart.js";
 import "./Dashboard.css";
 import jwtDecode from 'jwt-decode';
@@ -14,6 +20,8 @@ Chart.register(...registerables);
 const TraineeDashboard = () => {
 	const [cookies, setCookie, removeCookie] = useCookies([]);
 	const [isBar, setIsBar] = useState(true);
+	
+
 	const pieOrBar = () => {
 		isBar ? setIsBar(false) : setIsBar(true);
 	};
@@ -24,7 +32,27 @@ const TraineeDashboard = () => {
 	const [studentProfileImage, setstudentProfileImage] = useState(0);
 	const [lastMessage, setLastMessage] = useState("");
 	const [userSlackID,setUserSlackID] = useState("");
+	const refOne = useRef(null)
+		const [open, setOpen] = useState(false);
+
+
+		const [range, setRange] = useState([
+			{
+				startDate: startOfWeek(new Date()),
+				endDate: endOfWeek(new Date()),
+				key: "selection",
+			},
+		]);
+
+		const hideOnOutsideClick = (e) => {
+			if (refOne.current && !refOne.current.contains(e.target)) {
+				setOpen(false);
+			}
+		};
+
+
 	useEffect(() => {
+		document.addEventListener("click", hideOnOutsideClick, true);
 			let userPayload = cookies.trainee;
 			let decodedPayload = jwtDecode(userPayload);
 			const signedInUserEmail = decodedPayload.email;
@@ -37,7 +65,7 @@ const TraineeDashboard = () => {
 			}).then((res)=> res.json()).then((userSlackID)=> setUserSlackID(userSlackID))
 	}, []);
     useEffect(()=>{
-		fetch(`/api/studentProfileData/${userSlackID}`)
+		fetch(`/api/studentProfileData/${userSlackID}/${getUnixTime(range[0].startDate)}/${getUnixTime(range[0].endDate)}`)
 			.then((res) => res.json())
 			.then((data) => {
 				setStudentStats(data.messagesStatsForEachChannel);
@@ -48,11 +76,40 @@ const TraineeDashboard = () => {
 				setLastMessage(data.finalTime);
 			})
 			.catch((err) => console.log(err));
-    },[userSlackID])
+    },[userSlackID, range])
 	return (
 		<div className="Dashboard">
 			<Nav studentProfileImage={studentProfileImage} />
 			<Title traineeName={studentName} />
+			<div className="search-sort-buttons">
+				<section className="calendarWrap">
+					<input
+						value={`${format(range[0].startDate, "dd/MM/yyy")} to ${format(
+							range[0].endDate,
+							"dd/MM/yyy"
+						)}`}
+						className="inputBox"
+						onClick={() => setOpen((open) => !open)}
+						readOnly
+					/>
+
+					<div ref={refOne}>
+						{open && (
+							<DateRangePicker
+								className="calendarElement"
+								date={new Date()}
+								onChange={(item) => {
+									setRange([item.selection]);
+								}}
+								editableDateInputs={true}
+								moveRangeOnFirstSelection={false}
+								ranges={range}
+								maxDate={new Date()}
+							/>
+						)}
+					</div>
+				</section>
+			</div>
 			{isBar ? (
 				<BarChart channels={studentStats} />
 			) : (
