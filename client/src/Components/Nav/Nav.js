0e773dd853c99e625e8f73b1f734d7b170a8dbd8
michellejanay <mindyourphotosynthesis@gmail.com>
2023-03-16 13:23:28 +0000

    dashboard-heading: pic added, header updated

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -4,8 +4,9 @@ import MentorUl from "./MentorUl";
 import "./Nav.css";
 import { CgClose } from "react-icons/cg";
 import { RxHamburgerMenu } from "react-icons/rx";
+import SlackDashLogoPic from './SlackDashLogoPic.png'
 
-const Nav = ({studentProfileImage}) => {
+const Nav = ({ studentProfileImage }) => {
 	return (
 		<nav className="navbar">
 			<div className="container-fluid">
@@ -19,24 +20,39 @@ const Nav = ({studentProfileImage}) => {
 					<RxHamburgerMenu />
 				</button>
 
-				{window.location.pathname === "/students/table/view" ? (
-							<span className="site-header">DASHBOARD</span>
-						) : (
-							<span className="site-header">MY PROFILE</span>
-						)}
-				<img src={studentProfileImage} alt='profile-picture' className="profile-pic"/>
+				{window.location.pathname === "/mentordashboardloggedin" ? (
+					<span className="site-header">DASHBOARD</span>
+				) : (
+					<span className="site-header">MY PROFILE</span>
+				)}
+
+				{window.location.pathname === "/mentordashboardloggedin" ? (
+					<img
+						src={SlackDashLogoPic}
+						alt="profile-picture"
+						className="profile-pic-logo"
+					/>
+				) : (
+					<img
+						src={studentProfileImage}
+						alt="profile-picture"
+						className="profile-pic"
+					/>
+				)}
 
 				<div
 					className="offcanvas offcanvas-end text-bg-dark"
 					tabndex="-1"
 					id="offcanvasDarkNavbar"
-					aria-labelledby="offcanvasDarkNavbarLabel">
+					aria-labelledby="offcanvasDarkNavbarLabel"
+				>
 					<div className="offcanvas-header">
 						<CgClose
 							type="button"
 							className="btn=close"
 							data-bs-dismiss="offcanvas"
-							aria-label="Close"/>
+							aria-label="Close"
+						/>
 					</div>
 
 					<div className="offcanvas-body">
