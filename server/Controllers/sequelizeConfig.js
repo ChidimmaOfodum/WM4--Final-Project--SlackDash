7b699d99418e2f841922e4a4f1b20468c49e59f3
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-09-22 10:24:09 -0400

    Update sequelizeConfig.js

--- server/Controllers/sequelizeConfig.js
+++ server/Controllers/sequelizeConfig.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_INTERNAL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   ssl: true,
   dialectOptions: {
